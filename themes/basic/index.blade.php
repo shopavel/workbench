715108b655e983ef808aee8ea8ba36fa27c815c7
Laurence Roberts <lsjroberts@gmail.com>
2013-07-02 15:37:48 +0100

    Added basic theme folder

---
+++ themes/basic/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('content')
+    <div class="hero-unit">
+        <h1>Shopavel</h1>
+        <p>An ecommerce platform for developers built on Laravel 4</p>
+    </div>
+@stop
---
+++ themes/basic/layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Shopavel Basic Theme</title>
+
+    </head>
+    <body>
+        <header class="hero-unit">
+            <h2>Shopavel</h2>
+        </header>
+
+        <div class="container">
+            @yield('content')
+        </div>
+
+        <footer class="container">
+            <p>Shopavel - <a href="http://www.shopavel.org">www.shopavel.org</a></p>
+        </footer>
+    </body>
+</html>
