180e9458a4929ec12d920a2eec94b5ae660fa0b1
Laurence Roberts <lsjroberts@gmail.com>
2013-07-02 16:46:33 +0100

    Moved theme to public directory

---
+++ public/themes/basic/views/layout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Shopavel Basic Theme</title>
+
+        @stylesheets('application')
+    </head>
+    <body>
+        <header class="hero-unit">
+            <h2>Shopavel</h2>
+        </header>
+
+        <div class="container">
+            @yield('content')
+        </div>
+
+        <footer class="container">
+            <p>Shopavel - <a href="http://www.shopavel.org">www.shopavel.org</a></p>
+        </footer>
+    </body>
+</html>
