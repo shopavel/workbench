ae084e81f426aef10b251f83f5906c5813bad6c5
Laurence Roberts <lsjroberts@gmail.com>
2013-07-02 17:41:11 +0100

    Created local db config and products table

--- workbench/shopavel/products/src/Shopavel/Products/Product.php
+++ workbench/shopavel/products/src/Shopavel/Products/Product.php
@@ -2,6 +2,11 @@
 
 class Product extends \Eloquent {
 
-    
-    
+    /**
+     * The database table used by the model.
+     * 
+     * @var string
+     */
+    protected $table = 'products';
+
 }
