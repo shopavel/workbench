89632296ec54473aee31634f55bb8b6b9bd126cd
Laurence Roberts <lsjroberts@gmail.com>
2013-07-04 10:26:33 +0100

    Converting tabs to spaces

--- workbench/shopavel/loops/src/Shopavel/Loops/LoopsServiceProvider.php
+++ workbench/shopavel/loops/src/Shopavel/Loops/LoopsServiceProvider.php
@@ -4,46 +4,46 @@ use Illuminate\Support\ServiceProvider;
 
 class LoopsServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('shopavel/loops');
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('shopavel/loops');
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app['loops'] = $this->app->share(function($app)
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['loops'] = $this->app->share(function($app)
         {
             $manager = new LoopManager;
 
             return $manager;
         });
-	}
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 
 }
--- workbench/shopavel/products/src/Shopavel/Products/ProductsServiceProvider.php
+++ workbench/shopavel/products/src/Shopavel/Products/ProductsServiceProvider.php
@@ -5,32 +5,32 @@ use Illuminate\Database\Eloquent\Builder;
 
 class ProductsServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('shopavel/products');
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('shopavel/products');
 
-		include __DIR__.'/../../routes.php';
-	}
+        include __DIR__.'/../../routes.php';
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
         $this->app['products'] = $this->app->share(function($app)
         {
             $product = new Product;
@@ -53,20 +53,20 @@ class ProductsServiceProvider extends ServiceProvider {
             }
         });
 
-		$this->app['loops.products'] = $this->app->share(function($app) use ($loop)
+        $this->app['loops.products'] = $this->app->share(function($app) use ($loop)
         {
             return $loop;
         });
-	}
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('products', 'loops.products');
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('products', 'loops.products');
+    }
 
 }
