617a8b18df59fc8b8a92bcab387394e1900b1cd5
Laurence Roberts <lsjroberts@gmail.com>
2013-07-04 04:53:14 +0100

    Added some initial config options

--- workbench/shopavel/shopavel/src/config/config.php
+++ workbench/shopavel/shopavel/src/config/config.php
@@ -11,6 +11,66 @@ return array(
     | 
     */
 
-    'theme' => 'basic'
+    'theme' => 'basic',
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Categories
+    |--------------------------------------------------------------------------
+    |
+    | Shopavel allows you to categorise your products. You may specify if you
+    | are going to use categories here.
+    | 
+    | Options: "on", "off"
+    | 
+    */
+
+    'categories' => 'on',
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Variations
+    |--------------------------------------------------------------------------
+    | 
+    | Shopavel allows you to add variations to your products. You may specify
+    | if you are going to use features and variations here.
+    |
+    | Options: "on", "off"
+    | 
+    */
+
+    'variations' => 'on',
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Stock
+    |--------------------------------------------------------------------------
+    | 
+    | Shopavel allows you to track your stock to the degree to which you
+    | desire.
+    |
+    | Options: "off", "basic", "count", "batch"
+    | 
+    */
+    
+    'stock' => 'basic',
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Warehouses
+    |--------------------------------------------------------------------------
+    | 
+    | Shopavel allows you to assign stock to specific warehouses to help with
+    | tracking it's location and fulfillment.
+    |
+    | Options: "on", "off"
+    | 
+    */
+   
+    'warehouses' => 'off',
 
 );
